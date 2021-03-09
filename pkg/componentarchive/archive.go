29c4195fa69a3005e571250f31abb110c32283a0
Tim Schrodi <tim.schrodi@sap.com>
2021-03-09 07:42:22 +0100

    fix base url in generator

--- pkg/componentarchive/archive.go
+++ pkg/componentarchive/archive.go
@@ -101,7 +101,7 @@ func (o *BuilderOptions) Build(fs vfs.FileSystem) (*ctf.ComponentArchive, error)
 	cd.ComponentSpec.Version = o.Version
 	cd.Provider = cdv2.InternalProvider
 	cd.RepositoryContexts = make([]cdv2.RepositoryContext, 0)
-	if len(o.BaseUrl) == 0 {
+	if len(o.BaseUrl) != 0 {
 		cd.RepositoryContexts = []cdv2.RepositoryContext{
 			{
 				Type:    cdv2.OCIRegistryType,
