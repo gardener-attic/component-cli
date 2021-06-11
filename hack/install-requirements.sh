f52fdc7c36b07f74a231bcf8867db9ab5e740350
Tim Schrodi <tim.schrodi@sap.com>
2021-06-11 15:10:57 +0200

    add info to install gnu core utils on mac devices

--- hack/install-requirements.sh
+++ hack/install-requirements.sh
@@ -18,3 +18,21 @@ echo "> Install Registry test binaries"
 mkdir -p ${PROJECT_ROOT}/tmp/test/bin
 curl -L "https://github.com/gardener/component-cli/raw/registry/bin/registry-$(go env GOOS)-$(go env GOARCH)" --output ${PROJECT_ROOT}/tmp/test/bin/registry
 chmod +x ${PROJECT_ROOT}/tmp/test/bin/registry
+
+platform=$(uname -s)
+if [[ ${platform} == *"Darwin"* ]]; then
+  cat <<EOM
+You are running in a MAC OS environment!
+Please make sure you have installed the following requirements:
+- GNU Core Utils
+- GNU Tar
+- GNU Sed
+Brew command:
+$ brew install coreutils gnu-sed gnu-tar grep jq
+Please allow them to be used without their "g" prefix:
+$ export PATH=/usr/local/opt/coreutils/libexec/gnubin:\$PATH
+$ export PATH=/usr/local/opt/gnu-sed/libexec/gnubin:\$PATH
+$ export PATH=/usr/local/opt/gnu-tar/libexec/gnubin:\$PATH
+$ export PATH=/usr/local/opt/grep/libexec/gnubin:\$PATH
+EOM
+fi
