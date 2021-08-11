842d65d6835e091a7af8f2ed28945fa15b13dace
Johannes Schicktanz <johannes.schicktanz@sap.com>
2021-08-11 09:11:49 +0200

    adds ref normalization to exported client methods

--- ociclient/client.go
+++ ociclient/client.go
@@ -117,6 +117,12 @@ func (c *client) InjectCache(cache cache.Cache) error {
 }
 
 func (c *client) Resolve(ctx context.Context, ref string) (name string, desc ocispecv1.Descriptor, err error) {
+	refspec, err := oci.ParseRef(ref)
+	if err != nil {
+		return "", ocispecv1.Descriptor{}, fmt.Errorf("unable to parse ref: %w", err)
+	}
+	ref = refspec.String()
+
 	resolver, err := c.getResolverForRef(ctx, ref, transport.PullScope)
 	if err != nil {
 		return "", ocispecv1.Descriptor{}, err
@@ -125,6 +131,12 @@ func (c *client) Resolve(ctx context.Context, ref string) (name string, desc oci
 }
 
 func (c *client) GetManifest(ctx context.Context, ref string) (*ocispecv1.Manifest, error) {
+	refspec, err := oci.ParseRef(ref)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse ref: %w", err)
+	}
+	ref = refspec.String()
+
 	resolver, err := c.getResolverForRef(ctx, ref, transport.PullScope)
 	if err != nil {
 		return nil, err
@@ -147,6 +159,12 @@ func (c *client) GetManifest(ctx context.Context, ref string) (*ocispecv1.Manife
 }
 
 func (c *client) Fetch(ctx context.Context, ref string, desc ocispecv1.Descriptor, writer io.Writer) error {
+	refspec, err := oci.ParseRef(ref)
+	if err != nil {
+		return fmt.Errorf("unable to parse ref: %w", err)
+	}
+	ref = refspec.String()
+
 	reader, err := c.getFetchReader(ctx, ref, desc)
 	if err != nil {
 		return err
@@ -199,6 +217,12 @@ func (c *client) getFetchReader(ctx context.Context, ref string, desc ocispecv1.
 }
 
 func (c *client) PushManifest(ctx context.Context, ref string, manifest *ocispecv1.Manifest, options ...PushOption) error {
+	refspec, err := oci.ParseRef(ref)
+	if err != nil {
+		return fmt.Errorf("unable to parse ref: %w", err)
+	}
+	ref = refspec.String()
+
 	opts := &PushOptions{}
 	opts.Store = c.cache
 	opts.ApplyOptions(options)
