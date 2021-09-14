1a54365ea107a47c1ee8279d6a0c2691639248c4
Günther Wannenmacher <72857276+guewa@users.noreply.github.com>
2021-09-14 13:24:17 +0200

    Unit test create

--- pkg/commands/componentarchive/create_test.go
+++ pkg/commands/componentarchive/create_test.go
@@ -21,6 +21,7 @@ import (
 	"github.com/gardener/component-cli/pkg/commands/componentarchive"
 	cdv2 "github.com/gardener/component-spec/bindings-go/apis/v2"
 	"github.com/gardener/component-spec/bindings-go/codec"
+	"github.com/gardener/component-spec/bindings-go/ctf"
 )
 
 var _ = Describe("Create", func() {
@@ -38,21 +39,44 @@ var _ = Describe("Create", func() {
 		opts.Name = "example.com/component/name"
 		opts.Version = "v0.0.1"
 		opts.ComponentArchivePath = "./create-test"
-
-		fileName := "component-descriptor.yaml"
+		//opts.ComponentNameMapping = "urlPath"
+		opts.BaseUrl = "example.com/testurl"
 
 		err := testdataFs.Mkdir(opts.ComponentArchivePath, os.ModePerm)
 		Expect(err).ToNot(HaveOccurred())
 
 		Expect(opts.Run(context.TODO(), logr.Discard(), testdataFs)).To(Succeed())
 
-		data, err := vfs.ReadFile(testdataFs, filepath.Join(opts.ComponentArchivePath, fileName))
+		data, err := vfs.ReadFile(testdataFs, filepath.Join(opts.ComponentArchivePath, ctf.ComponentDescriptorFileName))
 		Expect(err).ToNot(HaveOccurred())
 
 		cd := &cdv2.ComponentDescriptor{}
 		Expect(codec.Decode(data, cd)).To(Succeed())
 		Expect(cd.Name).To(Equal(opts.Name), "component name should be the same")
 		Expect(cd.Version).To(Equal(opts.Version), "component version should be the same")
+
+		// Expect(cd.RepositoryContexts.Len() > 0).To(BeTrue(), "The repository contexts should return some data")
+		repoCtx := cd.RepositoryContexts[0]
+		Expect(repoCtx.GetType()).To(Equal(cdv2.OCIRegistryType), "check the repository context")
+
+		// check overwrite
+		opts.Version = "v0.0.2"
+		Expect(opts.Run(context.TODO(), logr.Discard(), testdataFs)).To(HaveOccurred(), "Should not overwrite existing component")
+
+		opts.Overwrite = true
+		Expect(opts.Run(context.TODO(), logr.Discard(), testdataFs)).To(Succeed(), "Should overwrite existing component")
+
+		data, err = vfs.ReadFile(testdataFs, filepath.Join(opts.ComponentArchivePath, ctf.ComponentDescriptorFileName))
+		Expect(err).ToNot(HaveOccurred())
+
+		cd = &cdv2.ComponentDescriptor{}
+		Expect(codec.Decode(data, cd)).To(Succeed())
+		Expect(cd.Name).To(Equal(opts.Name), "component name should be the same")
+		Expect(cd.Version).To(Equal(opts.Version), "component version should be the same")
+
+		repoCtx = cd.RepositoryContexts[0]
+		Expect(repoCtx.GetType()).To(Equal(cdv2.OCIRegistryType), "check the repository context")
+
 	})
 
 })
