2b2710ca9d3851d40e5cf258b5a8fbdcfe161df9
Johannes Schicktanz <johannes.schicktanz@sap.com>
2021-11-03 13:47:05 +0100

    updates license headers

--- pkg/transport/process/downloaders/local_oci_blob.go
+++ pkg/transport/process/downloaders/local_oci_blob.go
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors.
 //
-// SPDX-License-Identifier
+// SPDX-License-Identifier: Apache-2.0
 package downloaders
 
 import (
