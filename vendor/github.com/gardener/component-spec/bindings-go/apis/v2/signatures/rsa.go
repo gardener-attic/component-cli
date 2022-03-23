7d45eff04754e10ff67aa97e39053d0c8dca1f9c
Johannes Schicktanz <johannes.schicktanz@sap.com>
2022-03-23 16:55:30 +0100

    update component spec and vendor folder

--- vendor/github.com/gardener/component-spec/bindings-go/apis/v2/signatures/rsa.go
+++ vendor/github.com/gardener/component-spec/bindings-go/apis/v2/signatures/rsa.go
@@ -1,19 +1,19 @@
 package signatures
 
 import (
-	"crypto"
+	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/hex"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"strings"
 
 	v2 "github.com/gardener/component-spec/bindings-go/apis/v2"
 )
 
-// RsaSigner is a signatures.Signer compatible struct to sign with RSASSA-PKCS1-V1_5-SIGN.
+// RsaSigner is a signatures.Signer compatible struct to sign with RSASSA-PKCS1-V1_5.
 type RsaSigner struct {
 	privateKey rsa.PrivateKey
 }
@@ -30,10 +30,16 @@ func CreateRsaSignerFromKeyFile(pathToPrivateKey string) (*RsaSigner, error) {
 	if block == nil {
 		return nil, fmt.Errorf("failed decoding PEM formatted block in key %w", err)
 	}
-	key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	untypedPrivateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
 	if err != nil {
 		return nil, fmt.Errorf("failed parsing key %w", err)
 	}
+
+	key, ok := untypedPrivateKey.(*rsa.PrivateKey)
+	if !ok {
+		return nil, fmt.Errorf("parsed key is not of type *rsa.PrivateKey: %T", untypedPrivateKey)
+	}
+
 	return &RsaSigner{
 		privateKey: *key,
 	}, nil
@@ -45,35 +51,36 @@ func (s RsaSigner) Sign(componentDescriptor v2.ComponentDescriptor, digest v2.Di
 	if err != nil {
 		return nil, fmt.Errorf("failed decoding hash to bytes")
 	}
-	hashType, err := hashAlgorithmLookup(digest.HashAlgorithm)
-	if err != nil {
-		return nil, fmt.Errorf("failed looking up hash algorithm")
-	}
-	signature, err := rsa.SignPKCS1v15(nil, &s.privateKey, hashType, decodedHash)
+	signature, err := rsa.SignPKCS1v15(rand.Reader, &s.privateKey, 0, decodedHash)
 	if err != nil {
 		return nil, fmt.Errorf("failed signing hash, %w", err)
 	}
 	return &v2.SignatureSpec{
-		Algorithm: "RSASSA-PKCS1-V1_5-SIGN",
+		Algorithm: v2.SignatureAlgorithmRSAPKCS1v15,
 		Value:     hex.EncodeToString(signature),
+		MediaType: v2.MediaTypeRSASignature,
 	}, nil
 }
 
-// maps a hashing algorithm string to crypto.Hash
-func hashAlgorithmLookup(algorithm string) (crypto.Hash, error) {
-	switch strings.ToLower(algorithm) {
-	case SHA256:
-		return crypto.SHA256, nil
-	}
-	return 0, fmt.Errorf("hash Algorithm %s not found", algorithm)
-}
-
-// RsaVerifier is a signatures.Verifier compatible struct to verify RSASSA-PKCS1-V1_5-SIGN signatures.
+// RsaVerifier is a signatures.Verifier compatible struct to verify RSASSA-PKCS1-V1_5 signatures.
 type RsaVerifier struct {
 	publicKey rsa.PublicKey
 }
 
-// CreateRsaVerifierFromKeyFile creates an Instance of RsaVerifier with the given rsa public key.
+// CreateRsaVerifier creates an instance of RsaVerifier from a given rsa public key.
+func CreateRsaVerifier(publicKey *rsa.PublicKey) (*RsaVerifier, error) {
+	if publicKey == nil {
+		return nil, errors.New("public key must not be nil")
+	}
+
+	verifier := RsaVerifier{
+		publicKey: *publicKey,
+	}
+
+	return &verifier, nil
+}
+
+// CreateRsaVerifierFromKeyFile creates an instance of RsaVerifier from a rsa public key file.
 // The private key has to be in the PKIX, ASN.1 DER form, see x509.ParsePKIXPublicKey.
 func CreateRsaVerifierFromKeyFile(pathToPublicKey string) (*RsaVerifier, error) {
 	publicKey, err := ioutil.ReadFile(pathToPublicKey)
@@ -90,9 +97,7 @@ func CreateRsaVerifierFromKeyFile(pathToPublicKey string) (*RsaVerifier, error) 
 	}
 	switch key := untypedKey.(type) {
 	case *rsa.PublicKey:
-		return &RsaVerifier{
-			publicKey: *key,
-		}, nil
+		return CreateRsaVerifier(key)
 	default:
 		return nil, fmt.Errorf("public key format is not supported. Only rsa.PublicKey is supported")
 	}
@@ -100,21 +105,59 @@ func CreateRsaVerifierFromKeyFile(pathToPublicKey string) (*RsaVerifier, error) 
 
 // Verify checks the signature, returns an error on verification failure
 func (v RsaVerifier) Verify(componentDescriptor v2.ComponentDescriptor, signature v2.Signature) error {
+	var signatureBytes []byte
+	var err error
+	switch signature.Signature.MediaType {
+	case v2.MediaTypeRSASignature:
+		signatureBytes, err = hex.DecodeString(signature.Signature.Value)
+		if err != nil {
+			return fmt.Errorf("unable to get signature value: failed decoding hash %s: %w", signature.Digest.Value, err)
+		}
+	case v2.MediaTypePEM:
+		signaturePemBlocks, err := GetSignaturePEMBlocks([]byte(signature.Signature.Value))
+		if err != nil {
+			return fmt.Errorf("unable to get signature pem blocks: %w", err)
+		}
+		if len(signaturePemBlocks) != 1 {
+			return fmt.Errorf("expected 1 signature pem block, found %d", len(signaturePemBlocks))
+		}
+		signatureBytes = signaturePemBlocks[0].Bytes
+	default:
+		return fmt.Errorf("invalid signature mediaType %s", signature.Signature.MediaType)
+	}
+
 	decodedHash, err := hex.DecodeString(signature.Digest.Value)
 	if err != nil {
 		return fmt.Errorf("failed decoding hash %s: %w", signature.Digest.Value, err)
 	}
-	decodedSignature, err := hex.DecodeString(signature.Signature.Value)
-	if err != nil {
-		return fmt.Errorf("failed decoding hash %s: %w", signature.Digest.Value, err)
-	}
-	algorithm, err := hashAlgorithmLookup(signature.Digest.HashAlgorithm)
-	if err != nil {
-		return fmt.Errorf("failed looking up hash algorithm for %s: %w", signature.Digest.HashAlgorithm, err)
-	}
-	err = rsa.VerifyPKCS1v15(&v.publicKey, algorithm, decodedHash, decodedSignature)
-	if err != nil {
+	if err := rsa.VerifyPKCS1v15(&v.publicKey, 0, decodedHash, signatureBytes); err != nil {
 		return fmt.Errorf("signature verification failed, %w", err)
 	}
 	return nil
+}
+
+// GetSignaturePEMBlocks returns all signature pem blocks from a list of pem blocks
+func GetSignaturePEMBlocks(pemData []byte) ([]*pem.Block, error) {
+	if len(pemData) == 0 {
+		return []*pem.Block{}, nil
+	}
+
+	signatureBlocks := []*pem.Block{}
+	for {
+		var currentBlock *pem.Block
+		currentBlock, pemData = pem.Decode(pemData)
+		if currentBlock == nil && len(pemData) > 0 {
+			return nil, fmt.Errorf("unable to decode pem block %s", string(pemData))
+		}
+
+		if currentBlock.Type == v2.SignaturePEMBlockType {
+			signatureBlocks = append(signatureBlocks, currentBlock)
+		}
+
+		if len(pemData) == 0 {
+			break
+		}
+	}
+
+	return signatureBlocks, nil
 }
