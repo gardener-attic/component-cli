bfeec10a1878d68285c7bea8e73ef723c1e25946
Enrico Kaack <enrico.kaack@sap.com>
2022-05-19 16:16:04 +0200

    Update pkg/commands/componentarchive/signature/signature_test.go
    Co-authored-by: Johannes Schicktanz <johannes.schicktanz@sap.com>

--- pkg/commands/componentarchive/signature/signature_test.go
+++ pkg/commands/componentarchive/signature/signature_test.go
@@ -325,7 +325,7 @@ var _ = Describe("signature", func() {
 			err = verify.CheckCdDigests(digestedParentCd, *repoCtx, client, context.TODO())
 			Expect(err).To(HaveOccurred())
 		})
-		It("should fail verify with manipulated resource in parent", func() {
+		It("should fail to verify a digested cd with manipulated resource in parent", func() {
 			parentCd := getParentCd()
 			childCd := getChildCd()
 
